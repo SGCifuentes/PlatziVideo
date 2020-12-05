8b6f4f46c995a41331b9e8b94d7238d531921333
Sebastian Gonzalez <sebastiangc19.01@gmail.com>
2020-12-04 22:23:41 -0500

    Fixed styles for carouselItem

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 
 import playIcon from '../assets/static/play-icon.png';
@@ -9,8 +10,16 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <img src={cover} className='carousel-item__img' alt={title} />
     <div className='carousel-item__details'>
       <div>
-        <img className='carousel-item__details--img' src={playIcon} alt='play' />
-        <img className='carousel-item__details--img' src={plusIcon} alt='plus' />
+        <img
+          className='carousel-item__details--img'
+          src={playIcon}
+          alt='play'
+        />
+        <img
+          className='carousel-item__details--img'
+          src={plusIcon}
+          alt='plus'
+        />
       </div>
       <p className='carousel-item__details--title'>{title}</p>
       <p className='carousel-item__details--subtitle'>
@@ -19,5 +28,13 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
 
 export default CarouselItem;
